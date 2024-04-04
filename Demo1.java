c326ccd43aab5691536a183c5ea5fcd7550d6d0c
Aditya <ap7500223@gmail.com>
2024-04-04 18:40:46 +0530

    changes Variable

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,7 @@
 
 public class Demo1 {
 public static void main(String[] args) {
- String s="aditya id QA";
+ String s="aditya id QA Enginnerrr ";
  for (int i =s.length()-1 ; i>=0; i--) {
 	 if (s.charAt(i)==' ') {
 		System.out.print(" ");
