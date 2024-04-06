37a97288216683e803e40bfd9d820a893e8e3e54
Aditya <ap7500223@gmail.com>
2024-04-06 10:33:16 +0530

    test

---
+++ src/Today.java
@@ -0,0 +1,12 @@
+//prime
+public class Today {
+public static void main(String[] args) {
+	int n=10;
+	int count =0;
+    
+	
+			System.out.println(count++);
+			System.out.println(++count);
+			System.out.println(count);
+		}
+}
